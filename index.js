57539e5f31cd3b34a8b381e7395d485f5333da21
sadtakesh1 <sadtakeshi@yandex.ru>
2023-05-20 13:43:15 +0300

    new project commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const openPopupButtonEl = document.querySelector('#open-popup-button');
+const closePopupButtonEl = document.querySelector('#close-popup-button');
+
+const editPopupEl = document.querySelector('.popup');
+const pageTitleEl = document.querySelector('.profile__name');
+const pageAboutEl = document.querySelector('.profile__about');
+const nameInputEl = document.querySelector('#name-input');
+const nameAboutEl = document.querySelector('#name-input-second');
+const editFormEl = document.querySelector('#edit-form');
+
+openPopupButtonEl.addEventListener('click', function () {
+  openPopup(editPopupEl);
+});
+
+closePopupButtonEl.addEventListener('click', function () {
+  closePopup(editPopupEl);
+});
+
+nameInputEl.value = pageTitleEl.textContent;
+nameAboutEl.value = pageAboutEl.textContent;
+
+editFormEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  pageTitleEl.textContent = nameInputEl.value;
+
+    pageAboutEl.textContent = nameAboutEl.value;
+
+  closePopup(editPopupEl);
+});
+
+function openPopup(popupEl) {
+  popupEl.classList.add('popup_is-opened');
+}
+
+function closePopup(popupEl) {
+  popupEl.classList.remove('popup_is-opened');
+}
